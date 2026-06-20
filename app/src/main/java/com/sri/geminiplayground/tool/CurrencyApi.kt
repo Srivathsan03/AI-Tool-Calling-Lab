58062d373a470e567ef4bd762a56acfc190d6150
Srivathsan <srivathsanbe@gmail.com>
2026-06-20 13:18:20 +0530

    Refactor

--- app/src/main/java/com/sri/geminiplayground/tool/CurrencyApi.kt
+++ app/src/main/java/com/sri/geminiplayground/tool/CurrencyApi.kt
@@ -1,6 +1,6 @@
-package com.sri.geminiplayground.tool;
+package com.sri.geminiplayground.tool
 
-import retrofit2.http.GET;
+import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface CurrencyApi {
@@ -8,7 +8,7 @@ interface CurrencyApi {
     @GET("v1/latest")
     suspend fun convertCurrency(
         @Query("amount") amount: Double,
-        @Query("from") fromCurrency:String,
-        @Query("to") toCurrency:String
+        @Query("from") fromCurrency: String,
+        @Query("to") toCurrency: String
     ): ConvertCurrencyResp
 }
